c685ea7e2ff8ee9e91a19e37f91770e2baad340c
Jeremy Duenas <jeremy@duenas.dev>
2023-06-13 22:33:48 -0400

    Added laravel for backend

---
+++ backend/database/migrations/2023_06_14_022835_create_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('board_id');
+            $table->string('title', 100);
+            $table->text('content');
+            $table->integer('position_x')->default(0);
+            $table->integer('position_y')->default(0);
+            $table->timestamps();
+
+            $table->foreign('board_id')->references('id')->on('boards');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
